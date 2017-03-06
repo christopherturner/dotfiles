2d24574299e985eb4f35aa2992f0b920c4ee8db4
Christopher Matthew Turner <TurnerC@usc.edu>
2017-03-06 11:42:17 -0800

    Some homebrew basics

---
+++ brew.sh
@@ -0,0 +1,13 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+brew upgrade
+
+brew install coreutils
+brew install git
+brew install tor
+brew install tree
+brew install wget
+
+brew cleanup
